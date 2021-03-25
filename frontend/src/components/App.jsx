7c313cd14c5c94aec06169a72f227ac967cc6492
Irvin Meneses <developer@irvinemmanuel.xyz>
2021-03-24 21:15:14 -0500

    preparando el boilerplate del frontend

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,17 @@
+import "tailwindcss/tailwind.css"
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <svg />
+        <a href='mailto:developer@irvinemmanuel.xyz' target='_blank' rel='noreferrer'>
+          developer@irvinemmanuel.xyz
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
